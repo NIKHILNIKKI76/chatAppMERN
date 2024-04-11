ef958e09b22553fb5587bc7f28b98b31806520b7
nikhil <nikhilchary919@gmail.com>
2024-04-11 19:35:38 +0530

    2nd part completed -ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,44 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+		</div>
+	);
+};
+export default Conversations;
+
+// STARTER CODE SNIPPET
+// import Conversation from "./Conversation";
+
+// const Conversations = () => {
+// 	return (
+// 		<div className='py-2 flex flex-col overflow-auto'>
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 		</div>
+// 	);
+// };
+// export default Conversations;
