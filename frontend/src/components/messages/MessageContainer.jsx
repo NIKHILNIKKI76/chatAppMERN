a2147347df86a84a85a020bd2cf241a3e13fd4f4
nikhil <nikhilchary919@gmail.com>
2024-04-12 18:13:23 +0530

    inserter 'x' button in message container

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,6 +1,7 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import {TiMessages} from "react-icons/ti";
+import { TiMessages } from "react-icons/ti";
+import { IoCloseSharp } from "react-icons/io5";
 import useConversation from "../../zustand/useConversation";
 import React, { useEffect } from 'react';
 
@@ -18,30 +19,38 @@ const NoChatSelected = () => {
 };
 
 const MessageContainer = () => {
-	const {selectedConversation,setSelectedConversation} = useConversation();
+	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	useEffect(() => {
 		// cleanup function (unmounts)
 		return () => setSelectedConversation(null);
 	}, [setSelectedConversation]);
 	return (
-	  <div className='md:min-w-[450px] flex flex-col'>
-		{!selectedConversation ? (
-		  <NoChatSelected />
-		) : (
-		  <>
-			{/* Header */}
-			<div className='bg-slate-500 px-4 py-2 mb-2'>
-			  {/* <span className='label-text'>To:</span>{' '} */}
-			  <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-			</div>
-			<Messages />
-			<MessageInput />
-		  </>
-		)}
-	  </div>
+		<div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						{/* <span className='label-text'>To:</span>{' '} */}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+						<button
+							className='absolute top-0 right-0 px-3 py-2 mt-1 text-white'
+							onClick={() => setSelectedConversation(null)}
+						>
+							<IoCloseSharp />
+						</button>
+
+
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
 	);
-  };
+};
 
 export default MessageContainer;
 
