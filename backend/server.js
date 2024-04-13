dc3a3980115d962f5e90914fcf4efb9222620a9a
nikhil <nikhilchary919@gmail.com>
2024-04-13 13:43:28 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -15,6 +16,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());// to parse incomming request from req.json
 app.use(cookieParser());
 
@@ -22,10 +25,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.get("/",(req,res) => {
-    //root
-    res.send("Hello world");
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 })
+
+
 
 
 server.listen(PORT,()=>{
