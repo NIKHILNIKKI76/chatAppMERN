bf69cf9bab64975efa3c5f240a281cefb549c738
nikhil <nikhilchary919@gmail.com>
2024-04-14 09:55:18 +0530

    removing scrolling of website when using mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import { useAuthContext } from './context/AuthContext';
 function App() {
   const {authUser} = useAuthContext();
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
+    <div className='p-4 h-screen flex items-center justify-center no-scroll'>
       <Routes>
         <Route path='/' element={ authUser ? <Home/>: <Navigate to={"/login"} />} />
         <Route path='/login' element={ authUser ? <Navigate to='/'/>:<Login/> } />
